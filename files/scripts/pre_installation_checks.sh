59c46b7efb52b31e1f9f9471fc0dec90a2e6042d
Bromulux <lacroix.etienne@outlook.com>
2020-11-01 21:15:12 -0500

    partition menu

--- files/scripts/pre_installation_checks.sh
+++ files/scripts/pre_installation_checks.sh
@@ -63,61 +63,70 @@ show_disks_and_partitions(){
 }
 
 partition_menu(){
- if fdisk -l | grep -i efi; then
-    print_warning "There's already an EFI partition on that disk. Are you planing to dual boot?"
- else
-    print_info "ok"
- fi
+    
+        print_info "Launching partition menu"
+
+        printf "Enter Root partition disk:"
+        read -r root_location
+        printf "Root partition size (Gb):"
+        read -r root_size
+
+        printf "Enter Swap partition disk:"
+        read -r swap_location
+        printf "Swap partition size (Gb):"
+        read -r swap_size
+
+       printf "Enter EFI partition disk:"
+       read -r efi_location
+       printf "EFI partition size (Gb):"
+       read -r efi_size
+
+        printf "Enter Home partition disk:"
+        read -r home_location
+        printf "Home partition size (Gb):"
+        read -r home_size
+
+        create_partition "$efi_location" "efi" "$efi_size"
+        create_partition "$swap_location" "swap" "$swap_size"
+        create_partition "$root_location" "root" "$root_size"
+        create_partition "$home_location" "home" "$home_size"
 }
 
-#disk type size
+#disk type size 
 create_partition(){
-if is_gpt_parition_table_created "$1";then
-    case $2 in "efi") 
-     if is_efi_parition_created "$1"; then
-        echo "efi created"
-     else
-        echo "not created"
-     fi
-    ;;
-    "swap") 
-        printf "swap selected";;
-    "root") 
-        printf "root selected";;
-    "home") 
-        printf "home selected";;
-    *) 
-        printf "No supported partition type selected"
-        ;;
-    esac
-
-    
-    #if fdisk -l | grep -i efi; then
-    #    print_warning "There's already an EFI partition on that disk. Are you planing to dual boot? (y/n):"
-    #    read -r answer
-    #    if [ "$answer" = "n" ]; then
-    #     printf "exiting.."
-    #     exit 1
-    #    fi
-    #     print_info "skipping creation of EFI partition"
-    #else
-    #    printf 'o\nn\np\n1\n\n\nw' | fdisk /dev/sda
-    #fi
-else
-  print_warning "No GPT partition table was detected on $1. Would you like to wipe $1 and create the table? (y/n)"
-  read -r answer
-  if [ "$answer" = "n" ]; then
-         printf "exiting.."
-         exit 1
-  else
-   create_gpt_partition_table "$1" 2>/dev/null
-   create_partition "$1" "efi"
-  fi
-fi
+    if is_gpt_parition_table_created "$1";then
+        if [ "$1" = "efi" ]; then
+            if is_efi_parition_created "$1"; then
+                print_warning "There's already an EFI partition on that disk. Are you planing to dual boot? (y/n):"
+                read -r answer
+                if [ "$answer" = "n" ]; then
+                    printf "exiting.."
+                    exit 1
+                else
+                    print_info "skipping creation of EFI partition"
+                fi
+            fi
+        else
+            # (K,M,G,T,P)
+            { printf "n\n\n\n+%sG\n" "$3"; sleep 5; printf "w\n"; } | fdisk "$1" > /dev/null
+            sleep 5
+            change_last_partition_type "$1" "$2"
+            print_info "$2 partition created"
+        fi
+    else
+        print_warning "No GPT partition table was detected on $1. Would you like to wipe $1 and create the table? (y/n)"
+        read -r answer
+        if [ "$answer" = "n" ]; then
+            printf "exiting.."
+            exit 1
+        fi
+        create_gpt_partition_table "$1" 2> /dev/null
+        create_partition "$1" "$2" "$3"
+    fi
 }
 
 # when the disk has never been used or you want to clean it.
-# arguments: disk (/dev/sda)
+# disk
 create_gpt_partition_table(){
     # wtf is &> ?
     printf 'g\nw\n' | fdisk "$1" &> /dev/null
@@ -140,6 +149,29 @@ is_gpt_parition_table_created(){
       return 1
     fi
 }
+#disk type
+change_last_partition_type(){
+    EFI_TYPE_CODE="1"
+    SWAP_TYPE_CODE="19"
+    ROOT_TYPE_CODE="24"
+    HOME_TYPE_CODE="28"
+    
+    case $2 in "efi")
+        printf "t\n\n%s\nw\n" "$EFI_TYPE_CODE" | fdisk "$1" > /dev/null
+        ;;
+    "swap") 
+        printf "t\n\n%s\nw\n" "$SWAP_TYPE_CODE" | fdisk "$1" > /dev/null
+        ;;
+    "root") 
+        printf "t\n\n%s\nw\n" "$ROOT_TYPE_CODE" | fdisk "$1" > /dev/null
+        ;;
+    "home") 
+        printf "t\n\n%s\nw\n" "$HOME_TYPE_CODE" | fdisk "$1" > /dev/null
+        ;;
+    *) 
+        printf "No supported partition type selected";;
+    esac
+}
 
 partition_disk(){
     #https://superuser.com/questions/332252/how-to-create-and-format-a-partition-using-a-bash-script
@@ -151,4 +183,4 @@ pre-installation_checks
 update_system_clock
 clear
 show_disks_and_partitions
-create_partition "/dev/sda" "efi"
+partition_menu
