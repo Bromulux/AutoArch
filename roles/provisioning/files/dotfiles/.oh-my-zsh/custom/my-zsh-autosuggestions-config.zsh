41c923bbca55f80f14445e810d328855578065dd
Bromulux <lacroix.etienne@outlook.com>
2020-12-27 14:30:20 -0500

    WIP UI and rice setup

---
+++ roles/provisioning/files/dotfiles/.oh-my-zsh/custom/my-zsh-autosuggestions-config.zsh
@@ -0,0 +1,6 @@
+# Look here for more details: https://github.com/zsh-users/zsh-autosuggestions
+# in the configuration section (overriding internals)
+
+ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE="fg=242,bold"
+#exemple: "fg=#ff00ff,bg=cyan,bold,underline"
+
